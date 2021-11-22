ccde7700fb0947c0e78db6c1c040f963e98a3612
Fabio Beltramini <fabble@google.com>
2021-11-22 19:12:29 +0000

    Quote glob in lint command for more consistent behavior. Apply linter style to linter config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "jest/globals": true,
-        "es6": true,
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "extends": ["eslint:recommended", "google"],
-    "rules": {
-        "no-console": "off",
-        "max-len": 0,
-        "indent": ["error", "tab"],
-        "no-tabs": 0
-    },
-    "plugins": [
-        "jest"
-    ]
+	'env': {
+		'browser': true,
+		'node': true,
+		'jest/globals': true,
+		'es6': true,
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'ecmaFeatures': {
+			'jsx': true,
+		},
+		'sourceType': 'module',
+	},
+	'extends': ['eslint:recommended', 'google'],
+	'rules': {
+		'no-console': 'off',
+		'max-len': 0,
+		'indent': ['error', 'tab'],
+		'no-tabs': 0,
+	},
+	'plugins': [
+		'jest',
+	],
 };
