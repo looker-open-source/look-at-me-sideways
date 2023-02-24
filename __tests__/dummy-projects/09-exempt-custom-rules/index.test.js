ab4b31583d2f5f9846a1ef83323db4ffc3492bcd
Fabio Beltramini <fabble@google.com>
2023-02-24 10:47:58 -0500

    Note TODO in test

--- __tests__/dummy-projects/09-exempt-custom-rules/index.test.js
+++ __tests__/dummy-projects/09-exempt-custom-rules/index.test.js
@@ -16,6 +16,14 @@ describe('Projects', () => {
 		it("should not error out", ()=> {
 			expect(console.error).not.toHaveBeenCalled()
 		});
+		// TODO: Add this style of check. Requires updating of how exempt messages are counted in the custom rule evalator
+		// it("it should provide correct aggregate info (3 match, 1 exempt, 0 error)", ()=> {
+		// 	expect({messages}).toContainMessage({
+		// 		rule: "types_required",
+		// 		level: "info",
+		// 		description: "Evaluated 1 matches, with 1 exempt and 0 erroring"
+		// 	});
+		// });
 		it("it should not error on rule types_require for dimension:ok", ()=> {
 			expect({messages}).not.toContainMessage({
 				rule: "types_required",
