7d393df011b1e7499c86e7cddea878f97ccea034
Fabio Beltramini <fabble@google.com>
2023-08-10 02:18:25 -0400

    Rule E7 - Explore label length

---
+++ __tests__/e7.test.js
@@ -0,0 +1,107 @@
+/* Copyright (c) 2018 Looker Data Sciences, Inc. See https://github.com/looker-open-source/look-at-me-sideways/blob/master/LICENSE.txt */
+require('../lib/expect-to-contain-message');
+
+const rule = require('../rules/e7.js');
+const {parse} = require('lookml-parser');
+
+describe('Rules', () => {
+	describe('E7', () => {
+		let info = {level: 'info'};
+		let error = {level: 'error'};
+		let e7 = {rule: 'E7'};
+
+		it('should not match and pass if there are no models', () => {
+			let result = rule(parse(`file: foo{}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 0 matches, 0 matches exempt, and 0 errors"
+			});
+			expect(result).not.toContainMessage(error);
+		});
+
+		it('should not match and pass if there are no explores', () => {
+			let result = rule(parse(`model: foo {}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 0 matches, 0 matches exempt, and 0 errors"
+			});
+			expect(result).not.toContainMessage(error);
+		});
+
+		it('should pass concise un-labeled explore names', () => {
+			let result = rule(parse(`model: m {
+				explore: orders {}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 0 errors"
+			});
+			expect(result).not.toContainMessage(error);
+		});
+
+		it('should fail long un-labeled explore names', () => {
+			let result = rule(parse(`model: m {
+				explore: datawarehouse_bigquery__schema_business__datamart_ecommerce__facts__orders {}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 1 errors"
+			});
+			expect(result).toContainMessage({...e7, ...error});
+		});
+
+		it('should pass concise explore labels', () => {
+			let result = rule(parse(`model: m {
+				explore: orders {label: "All Orders"}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 0 errors"
+			});
+			expect(result).not.toContainMessage(error);
+		});
+
+		it('should fail long explore labels', () => {
+			let result = rule(parse(`model: m {
+				explore: orders {
+					label:"Data Warehouse: BigQuery > Schema: Business > Datamart: eCommerce > Facts > Orders"
+				}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 1 errors"
+			});
+			expect(result).toContainMessage({...e7, ...error});
+		});
+
+		it('should pass long explore names with concise labels', () => {
+			let result = rule(parse(`model: m {
+				explore: datawarehouse_bigquery_schema_business_datamart_ecommerce__facts__orders {
+					label: "All Orders"
+				}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 0 errors"
+			});
+			expect(result).not.toContainMessage(error);
+		});
+
+		it('should fail concise un-labeled explore names, if optional maxLength is shorter', () => {
+			let result = rule(parse(`
+			manifest: {rule: E7 {options: {maxLength: 5}}}
+			model: m {
+				explore: orders {}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 1 errors"
+			});
+			expect(result).toContainMessage({...e7, ...error});
+		});
+
+		it('should pass long un-labeled explore names, if optional maxLength is longer', () => {
+			let result = rule(parse(`
+			manifest: {rule: E7 {options: {maxLength: 75}}}
+			model: m {
+				explore: datawarehouse_bigquery__schema_business__datamart_ecommerce__facts__orders {}
+			}`));
+			expect(result).toContainMessage({...e7, ...info,
+				description: "Rule E7 summary: 1 matches, 0 matches exempt, and 0 errors"
+			});
+			expect(result).not.toContainMessage(error);
+		});
+	});
+});
---
+++ rules/e7.js
@@ -0,0 +1,29 @@
+/* Copyright (c)  Looker Data Sciences, Inc. See https://github.com/looker-open-source/look-at-me-sideways/blob/master/LICENSE.txt */
+
+const checkCustomRule = require('../lib/custom-rule/custom-rule.js');
+
+module.exports = function(
+	project,
+) {
+	let ruleDef = {
+		$name: 'E7',
+		match: `$.model.*.explore.*`,
+		ruleFn,
+	};
+	let messages = checkCustomRule(ruleDef, project, {ruleSource: 'internal'});
+
+	return {messages};
+};
+
+function ruleFn(match, path, project, options={}) {
+	const explore = match;
+	const label = explore.label
+		? {source:"label", text:explore.label}
+		: {source:"name",  text:explore.$name};
+	const length = label.text.length;
+	const maxLength = options.maxLength || 25;
+	if(length > maxLength){
+		return `Explore ${label.source}'s length (${length}) exceeds allowed length (${maxLength})`;
+	}
+	return true;
+}
