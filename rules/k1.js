97981a891405aff0c1ac0dbf7d9c39c0ed55e780
Micah Stubbs <hi@micah.fyi>
2024-09-03 17:51:25 -0700

    convetion --> convention

--- rules/k1.js
+++ rules/k1.js
@@ -1,53 +1,55 @@
 /* Copyright (c) Looker Data Sciences, Inc. See https://github.com/looker-open-source/look-at-me-sideways/blob/master/LICENSE.txt */
 
-const checkCustomRule = require('../lib/custom-rule/custom-rule.js');
-const pkNamingConvention = require('./rules-lib/pk-naming-convention.js');
+const checkCustomRule = require("../lib/custom-rule/custom-rule.js");
+const pkNamingConvention = require("./rules-lib/pk-naming-convention.js");
 
-module.exports = function(
-	project,
-) {
-	let ruleDef = {
-		$name: 'K1',
-		match: `$.model.*.view.*`,
-		ruleFn,
-	};
-	let messages = checkCustomRule(ruleDef, project, {ruleSource: 'internal'});
+module.exports = function (project) {
+  let ruleDef = {
+    $name: "K1",
+    match: `$.model.*.view.*`,
+    ruleFn,
+  };
+  let messages = checkCustomRule(ruleDef, project, { ruleSource: "internal" });
 
-	return {messages};
+  return { messages };
 };
 
 function ruleFn(match, path, project) {
-	let view = match;
+  let view = match;
 
-	const unique = (x, i, arr) => arr.indexOf(x) === i;
+  const unique = (x, i, arr) => arr.indexOf(x) === i;
 
-	/* Skip field-only views */
-	if (!view.derived_table && !view.sql_table_name) {
-		return {
-			level: 'verbose',
-			description: `Field-only view ${view.$name} is not subject to Primary Key Dimension rule K1`,
-		};
-	}
+  /* Skip field-only views */
+  if (!view.derived_table && !view.sql_table_name) {
+    return {
+      level: "verbose",
+      description: `Field-only view ${view.$name} is not subject to Primary Key Dimension rule K1`,
+    };
+  }
 
-	let pkDimensions = (Object.values(view.dimension || {})).filter(pkNamingConvention);
+  let pkDimensions = Object.values(view.dimension || {}).filter(
+    pkNamingConvention
+  );
 
-	if (!pkDimensions.length) {
-		return `View ${view.$name} has no dimensions that follow the PK naming convetion`;
-	}
+  if (!pkDimensions.length) {
+    return `View ${view.$name} has no dimensions that follow the PK naming convention`;
+  }
 
-	let declaredNs = pkDimensions
-		.map(pkNamingConvention)
-		.map((match) => match[1].replace('pk', ''))
-		.map((n) => (n===''?'1':n))
-		.filter(unique);
-	if (declaredNs.length > 1) {
-		return `Different PK dimensions in ${view.$name} declare different column counts: ${declaredNs.join(', ')}`;
-	}
+  let declaredNs = pkDimensions
+    .map(pkNamingConvention)
+    .map((match) => match[1].replace("pk", ""))
+    .map((n) => (n === "" ? "1" : n))
+    .filter(unique);
+  if (declaredNs.length > 1) {
+    return `Different PK dimensions in ${
+      view.$name
+    } declare different column counts: ${declaredNs.join(", ")}`;
+  }
 
-	let n = parseInt(declaredNs[0]);
-	if (n != pkDimensions.length && n !== 0) {
-		return `View ${view.$name} has ${pkDimensions.length} PK dimension(s) but their names declare ${declaredNs[0]} columns`;
-	}
+  let n = parseInt(declaredNs[0]);
+  if (n != pkDimensions.length && n !== 0) {
+    return `View ${view.$name} has ${pkDimensions.length} PK dimension(s) but their names declare ${declaredNs[0]} columns`;
+  }
 
-	return true;
+  return true;
 }
